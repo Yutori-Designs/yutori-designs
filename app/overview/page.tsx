d3ab75a7e0572a09186b9a92dd42c868fe300fa3
Pavan <pavanachar0123@gmail.com>
2026-07-16 10:31:14 +0530

    image size adujstment

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -51,7 +51,7 @@ export default function OverviewPage() {
 
       <section className="relative h-[280px] sm:h-[360px] lg:h-[420px] overflow-hidden">
         <Image
-          src="/images/brand/Yutori-Tumbnail.png"
+          src="/images/brand/tumbnail.jpg"
           alt="A Yutori Designs interior"
           fill
           className="object-cover object-center"
